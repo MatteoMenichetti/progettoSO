d991417d07c0371c4ddd646a54869397ddb85208
matteo <matteo.menichetti@stud.unifi.it>
2021-05-19 10:53:15 +0200

    aggiunta sequenza avvio

--- src/main.c
+++ src/main.c
@@ -2,5 +2,13 @@
 
 int main() {
     printf("Hello, World!\n");
+
+    /*
+     * avvio processi
+     * P1, P2, P3
+     * decision_function
+     * failure_manager
+     * input_manager
+     */
     return 0;
 }
