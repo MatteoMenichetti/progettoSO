baf3eac09c77d259ae8dfe01df881bb9c7a1762e
matteo <matteo.menichetti@stud.unifi.it>
2021-05-19 12:38:08 +0200

    introdotto comportamento iniziale p

---
+++ lib/ipc.h
@@ -0,0 +1,9 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+#define PADDR "pipe"
---
+++ src/p.c
@@ -0,0 +1,32 @@
+#include "../lib/ipc.h"
+
+int main(int argc, int *argv[]) {
+    if (argc < 3) {
+        printf("P: argomenti non sufficienti");
+        exit(0);
+    }
+
+
+    if (*argv[2] == 1) {
+        if (!fork())
+            p1(*argv[1]);
+    }
+    if (*argv[2] == 2) {
+        if (!fork())
+            p2(*argv[1]);
+    }
+    if (*argv[2] == 3) {
+        if (!fork())
+            p3(*argv[1]);
+    }
+    return 0;
+}
+
+void p1(int mode) {
+    int pfd = open(PADDR, O_RDONLY);
+    char buff[100];
+    while (0 == 0) {
+        read(pfd, buff, 100)
+        printf("P1: legge = %s", buff);
+    }
+}
