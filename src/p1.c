8b0f1cfbc3c1216ac18be0eedc9e5fd7b9feff9d
matteo <matteo.menichetti@stud.unifi.it>
2021-05-22 09:16:13 +0200

    Comportamento P1 aggiunto

--- src/p1.c
+++ src/p1.c
@@ -1,20 +1,25 @@
 #include "../lib/p.h"
 
-void p1(int modality, int sfd) {
+void p1(int modality, int pdfd) {
     int pfd = open(PIPEADDR, O_RDONLY);
-    char buff[100];
+    char buff[BUFSIZ];
     while (0 == 0) {
-        read(pfd, buff, 100);
+        read(pfd, buff, sizeof(buff));
         printf("P1: legge = %s\n", buff);
-        split(buff);
+        split(buff, pdfd);
     }
 }
 
-char *split(char *buff) {
+void split(char *buff, pdfd) {
     char *token = strtok(buff, delim);
+    int s;
     do {
-        printf("token = %s ", token);
-        int s=sum(token, 0);
+        printf("P1: token = %s\n", token);
+        s = sum(token, 0);
+        printf("P1: invio a DF %d\n", s);
+        if (write(pdfd, &s, sizeof(s)) == -1) {
+            perror("P1: write");
+            exit(EXIT_FAILURE);
+        }
     } while ((token = strtok(NULL, delim)) != NULL);
-    return NULL;
 }
