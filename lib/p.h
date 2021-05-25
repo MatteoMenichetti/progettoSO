a9e9e2177ad95ddc1302c81fa4fe642a68862d39
matteo <matteo.menichetti@stud.unifi.it>
2021-05-25 10:46:24 +0200

    rimossi parametri per definire err in p a seconda del valore ricevuto (FALLIMENTO O NORMALE)

--- lib/p.h
+++ lib/p.h
@@ -4,15 +4,16 @@
 #define ENABLE "FALLIMENTO"
 
 #define INIT_ERR srand(time(NULL))
-#define ERR rand() % (10) + 1
+
 
 void splitP1(char *, int);
+
 void splitP2(char *, int);
 
 int sum(char *);
 
-void p1(int, int);
+void p1(int);
 
-void p2(int, int );
+void p2(int);
 
-void p3(int, int);
+void p3(int);
