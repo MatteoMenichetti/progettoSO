ec007946da4bbb65f3c100ca7a174fbc72434ff3
matteo <matteo.menichetti@stud.unifi.it>
2021-05-25 10:42:14 +0200

    aggiunta gestione errore

--- lib/p.h
+++ lib/p.h
@@ -3,10 +3,13 @@
 #define delim ','
 #define ENABLE "FALLIMENTO"
 
+#define INIT_ERR srand(time(NULL))
+#define ERR rand() % (10) + 1
+
 void splitP1(char *, int);
 void splitP2(char *, int);
 
-int sum(char *, int);
+int sum(char *);
 
 void p1(int, int);
 
