33c6c821f9502679d1cf6d0c5c17cb991dccca47
matteo <matteo.menichetti@stud.unifi.it>
2021-05-26 16:00:59 +0200

    definizione macro riguardo posizione del logfile e posizioni assegnate ai canali di comunicazione con i processi e log. aggiunto metodo per aprire i canali  ed il file di log

--- src/decision_function.c
+++ src/decision_function.c
@@ -1,18 +1,40 @@
 #include "../lib/ipc.h"
 #include <stdlib.h>
 
-int main(void) {
-    int pfd;
-    if ((pfd = open(PIPEDECISIONADDR, O_RDONLY) == -1)) {
+#define LOGPOS "system_log"
+#define LOGVAL 0
+#define P1 1
+#define P2 2
+#define P3 3
+
+void opens(int *fd) {
+
+    if ((fd[LOGVAL] = open(LOGPOS, O_WRONLY) == -1)) {
         perror("DF: open");
         exit(EXIT_FAILURE);
     }
+    if ((fd[P1] = open(PIPEDP1, O_RDONLY) == -1)) {
+        perror("DF: open");
+        exit(EXIT_FAILURE);
+    }
+    if ((fd[P2] = open(PIPEDP2, O_RDONLY) == -1)) {
+        perror("DF: open");
+        exit(EXIT_FAILURE);
+    }
+    if ((fd[P3] = open(PIPEDP3, O_RDONLY) == -1)) {
+        perror("DF: open");
+        exit(EXIT_FAILURE);
+    }
+}
 
+int main(void) {
+    int fd[4];
     char buff[BUFSIZ];
+    opens(fd);
     while (0 == 0) {
         for (int i = 0; i < 3; i++) {
             printf("DF: eseguo la read\n");
-            if (read(pfd, (buff + i), sizeof(int)) == -1) {
+            if (read(fd[P1], (buff + i), sizeof(int)) == -1) {
                 perror("DF: read");
                 exit(EXIT_FAILURE);
             }
