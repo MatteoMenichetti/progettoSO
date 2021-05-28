8806ea16c38ca1b2ec2b8e502a86d3f242f8e407
matteo <matteo.menichetti@stud.unifi.it>
2021-05-28 14:54:10 +0200

    aggiunta print segnalazione

--- src/decision_function.c
+++ src/decision_function.c
@@ -5,18 +5,24 @@
 #define LOGVALUE 0
 
 void opens(int *fd) {
+    printf("DF: apertura LOGILE\n");
     if ((fd[LOGVALUE] = open(LOGPOS, O_WRONLY | O_CREAT, 0666) == -1)) {
         perror("DF: open log");
         exit(EXIT_FAILURE);
     }
+    printf("DF: apertura PIPEDP1\n");
     if ((fd[P1] = open(PIPEDP1, O_RDONLY) == -1)) {
         perror("DF: open pipedp1");
         exit(EXIT_FAILURE);
     }
+
+    printf("DF: apertura PIPEDP2\n");
     if ((fd[P2] = open(PIPEDP2, O_RDONLY) == -1)) {
         perror("DF: open pipedp2");
         exit(EXIT_FAILURE);
     }
+
+    printf("DF: apertura PIPEDP3\n");
     if ((fd[P3] = open(PIPEDP3, O_RDONLY) == -1)) {
         perror("DF: open pipedp3");
         exit(EXIT_FAILURE);
