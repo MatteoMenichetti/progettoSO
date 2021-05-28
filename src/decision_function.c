aa6907bb56cf5be74b27d118eb13205aa1bc6c12
matteo <matteo.menichetti@stud.unifi.it>
2021-05-28 12:33:48 +0200

    spostati le macro relative a p e introdotto inizio comportamento di decision_function

--- src/decision_function.c
+++ src/decision_function.c
@@ -1,44 +1,37 @@
 #include "../lib/ipc.h"
 #include <stdlib.h>
 
-#define LOGPOS "system_log"
-#define LOGVAL 0
-#define P1 1
-#define P2 2
-#define P3 3
+#define LOGPOS "../logs/system_log"
+#define LOGVALUE 0
 
 void opens(int *fd) {
-
-    if ((fd[LOGVAL] = open(LOGPOS, O_WRONLY) == -1)) {
-        perror("DF: open");
+    if ((fd[LOGVALUE] = open(LOGPOS, O_WRONLY | O_CREAT, 0666) == -1)) {
+        perror("DF: open log");
         exit(EXIT_FAILURE);
     }
     if ((fd[P1] = open(PIPEDP1, O_RDONLY) == -1)) {
-        perror("DF: open");
+        perror("DF: open pipedp1");
         exit(EXIT_FAILURE);
     }
     if ((fd[P2] = open(PIPEDP2, O_RDONLY) == -1)) {
-        perror("DF: open");
+        perror("DF: open pipedp2");
         exit(EXIT_FAILURE);
     }
     if ((fd[P3] = open(PIPEDP3, O_RDONLY) == -1)) {
-        perror("DF: open");
+        perror("DF: open pipedp3");
         exit(EXIT_FAILURE);
     }
 }
 
 int main(void) {
-    int fd[4];
-    char buff[BUFSIZ];
+    int fd[4], nr;
+    char *vp1, *vp2, *vp3;
     opens(fd);
     while (0 == 0) {
-        for (int i = 0; i < 3; i++) {
-            printf("DF: eseguo la read\n");
-            if (read(fd[P1], (buff + i), sizeof(int)) == -1) {
-                perror("DF: read");
-                exit(EXIT_FAILURE);
-            }
-            printf("DF: buff[i] = %c\n", buff[i]);
-        }
+        printf("DF: leggo\n");
+        if (read(fd[P1], vp1, sizeof(int)) == -1)perror("DF: read P1");
+        if (read(fd[P2], vp2, sizeof(int)) == -1)perror("DF: read P2");
+        if (read(fd[P3], vp3, sizeof(int)) == -1)perror("DF: read P3");
+
     }
 }
