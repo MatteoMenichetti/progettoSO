ebb1c8bcc6a4273b742fb409be3c0e2992b915f9
matteo <matteo.menichetti@stud.unifi.it>
2021-05-28 10:24:15 +0200

    Aggiunte macro e metodo per gestione Fallimento

--- lib/p.h
+++ lib/p.h
@@ -1,17 +1,22 @@
 #include "../lib/ipc.h"
 #include <time.h>
 
+#define ERR rand() % (10) + 1
 #define delim ((const char*)',')
 #define ENABLE "FALLIMENTO"
+#define EVENT 0
+#define ACTIVE_FAILURE 1
 
-void splitP1(char *, int);
+void splitP1(char *, int, int);
 
-void splitP2(char *, int);
+void splitP2(char *, int, int, int, int);
 
-int sum(char *);
+int sum(char *, int);
 
-void p1(int);
+void p1(int,int);
 
-void p2(int);
+void p2(int, int);
 
-void p3(int);
+void p3(int,int);
+
+void errsum(int *, int);
