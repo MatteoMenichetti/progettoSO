a9330db7de02f93dd3de466b33424f25a53db2b4
matteo <matteo.menichetti@stud.unifi.it>
2021-06-03 15:12:46 +0200

    aggiunto avvio processi

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char **argv) {
     printf("Hello, World!\n");
 
     /*
@@ -8,5 +9,10 @@ int main() {
      * input manager avvia P1,  P2, P3
      * decision_function avvia failure_manager
      */
+
+    if (!fork())
+        execl("./input_manager", "./input_manager", *argv[1], NULL);
+    if (!fork())
+        execl("./decision_function", "./decision_function", NULL);
     return 0;
 }
