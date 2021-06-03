82fb47b65ce9e740d3bbc6f4ee9bb924d9d0c3f4
matteo <matteo.menichetti@stud.unifi.it>
2021-06-03 15:33:48 +0200

    .

--- src/p3.c
+++ src/p3.c
@@ -17,25 +17,34 @@ void p3(int flag) {
     }
 
     printf("P3: eseguo fopen\n");
-
-    FILE *fd = fopen(FILEADDR, "r");
+    FILE *fd = fopen(FILEADDR, "r+");
 
     printf("P3: fopen eseguita\n");
     if (fd == NULL) {
         perror("P3: open");
         exit(EXIT_FAILURE);
     }
-    char buff[BUFSIZ], *token = (char *) calloc(1, sizeof(char));
-    int s = 0;
+    printf("P3: sono prima del buffer \n");
+    char buff[BUFSIZ];
+    char *token = (char *) calloc(1, sizeof(char));
+    printf("P3: sono prima del while \n");
+
+    int s;
+    int res = 0;
+
     while (0 == 0) {
-        while ((int) *fgets(buff, sizeof(buff), fd) == EOF)usleep(500);
-        printf("P3: buff = %s", buff);
-        token = splitP2(buff);
-        s = sum(token, 0);
-        if (write(psfd, &s, sizeof(s)) == -1) {
-            perror("P1: write");
-            exit(EXIT_FAILURE);
+        if (fgets(buff, sizeof(buff), fd)) {
+            res += strlen(buff);
+            token = splitP2(buff);
+            s = sum(token, 0);
+            errsum(&s, 30);
+            printf("somma: %d \n", s);
+            if (write(psfd, &s, sizeof(s)) == -1) {
+                perror("P1: write");
+                exit(EXIT_FAILURE);
+            }
+            printf("Inviato: %d \n", s);
         }
-        strncpy(buff, "\0", strlen(buff));
+        fseek(fd, res, SEEK_SET);
     }
 }
