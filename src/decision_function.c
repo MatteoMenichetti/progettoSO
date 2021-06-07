6a25b39edf0c6b129c6562661b3c82244163b913
matteo <matteo.menichetti@stud.unifi.it>
2021-06-07 17:22:00 +0200

    introduzione funzione per le condizioni di uguaglianza tra i valori ricevuti dai p, creazione di un vettore per gestire i valori ricevuti dai p

--- src/decision_function.c
+++ src/decision_function.c
@@ -33,27 +33,31 @@ void openPIPE(int *fd) {
 
 }
 
+int EqualCondition(int vp1, int vp2, int vp3) {
+    if (!(vp1 == vp2) && !(vp1 == vp3) && !(vp2 == vp3))return 0;
+    return 1;
+}
 
 int main(void) {
-    int fd[4], pid,vp1, vp2, vp3;
+    int fd[4], pid, vp[3];
     if (!(pid = fork()))
-        execl("./failure_manager", "./failure_manager", NULL);
+        execl("./failure_manager", "failure_manager", NULL);
     openPIPE(fd);
     int i = 1;
     int voted_output = open(VOTEDO, O_WRONLY | O_CREAT | O_TRUNC, 0666);
     while (0 == 0) {
-        if ((read(fd[P1], &vp1, sizeof(int))) == -1) { perror("DF: read P1"); }
+        if ((read(fd[P1], (vp + P1 - 1), sizeof(int))) == -1) { perror("DF: read P1"); }
 
-        if ((read(fd[P2], &vp2, sizeof(int))) == -1) { perror("DF: read P2"); }
+        if ((read(fd[P2], (vp + P2 - 1), sizeof(int))) == -1) { perror("DF: read P2"); }
 
-        if ((read(fd[P3], &vp3, sizeof(int))) == -1) { perror("DF: read P3"); }
+        if ((read(fd[P3], (vp + P3 - 1), sizeof(int))) == -1) { perror("DF: read P3"); }
         dprintf(voted_output, "inizio iterazione riga %d \n", i);
-        dprintf(voted_output, " p1: %d \n", vp1);
-        dprintf(voted_output, " p2: %d \n", vp2);
-        dprintf(voted_output, " p3: %d \n", vp3);
+        dprintf(voted_output, " p1: %d \n", vp + P1 - 1);
+        dprintf(voted_output, " p2: %d \n", vp + P2 - 1);
+        dprintf(voted_output, " p3: %d \n", vp + P3 - 1);
         dprintf(voted_output, "fine iterazione riga %d \n", i);
 
-        if (!(vp1 == vp2) && !(vp1 == vp3) && !(vp2 == vp3)) {
+        if (!EqualCondition(vp[P1 - 1], vp[P2 - 1], vp[P3 - 1])) {
             writeOnLog(fd[LOGVALUE], FALLIMENTO);
             kill(pid, SIGUSR1);
         } else {
