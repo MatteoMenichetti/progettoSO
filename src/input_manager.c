e1524c6799aa4b0354b61d5cc63af784ca45fdb0
matteo <matteo.menichetti@stud.unifi.it>
2021-06-08 12:00:33 +0200

    rinominazione file descriptor socket, formattato codice e rimossa variabile inutilizzata

--- src/input_manager.c
+++ src/input_manager.c
@@ -1,45 +1,51 @@
 #include "../lib/ipc.h"
 
 int initializationSOCKET(struct sockaddr_un *sockServer) {
+    int fd;
+    
     strcpy(sockServer->sun_path, SOCKADDR);
     sockServer->sun_family = AF_UNIX;
+    
     unlink(SOCKADDR);
-    int ssfd;
-    if ((ssfd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-        perror("server: socket");
+    
+    if ((fd = socket(AF_UNIX, SOCK_STREAM, DEFAULT)) == -1) {
+        perror("IM: socket");
         exit(EXIT_FAILURE);
     }
 
-    if (bind(ssfd, (struct sockaddr *) sockServer, sizeof(*sockServer)) == -1) {
-        perror("server: bind");
+    if (bind(fd, (struct sockaddr *) sockServer, sizeof(*sockServer)) == -1) {
+        perror("IM: bind");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(ssfd, 1) == -1) {
-        perror("server: listen");
+    if (listen(fd, 1) == -1) {
+        perror("IM: listen");
         exit(EXIT_FAILURE);
     }
 
-    return ssfd;
+    return fd;
 }
 
-void acceptSOCKET(int *asfd) {
+void acceptSOCKET(int *sfd) {
     struct sockaddr_un sockServer, socketClient;
     int ssfd = initializationSOCKET(&sockServer);
     unsigned int len = sizeof(socketClient);
-    if ((*asfd = accept(ssfd, (struct sockaddr *) &socketClient, &len)) == -1) {
+    
+    if ((*sfd = accept(ssfd, (struct sockaddr *) &socketClient, &len)) == -1) {
         perror("server: accept");
         exit(EXIT_FAILURE);
     }
 }
 
 void initializationPIPE() {
+    
     unlink(PIPEADDR);
+    
     if (mknod(PIPEADDR, S_IFIFO, DEFAULT) == -1) {
         perror("Input manager: PIPEADDR");
         exit(EXIT_FAILURE);
     }
-    chmod(PIPEADDR, 0777);
+    chmod(PIPEADDR, PERMISSION);
 }
 
 void openPIPE(int *fdPipe) {
@@ -54,22 +60,21 @@ void openPIPE(int *fdPipe) {
 int main(int argc, char *argv[]) {
 
     char buff[BUFSIZ];
-    int fdPipe, fpAppoggio = open(FILEADDR, O_CREAT | O_WRONLY | O_TRUNC, 0777), asfd;
+    int fdPipe, fpAppoggio = open(FILEADDR, O_CREAT | O_WRONLY | O_TRUNC, PERMISSION), sfd;
     FILE *fpData = fopen(argv[0], "r");
 
     initializationPIPE();
     openPIPE(&fdPipe);
-    acceptSOCKET(&asfd);
+    acceptSOCKET(&sfd);
 
     fgets(buff, BUFSIZ, fpData);
     strncpy(buff, "\0", strlen(buff));
 
-    int stringaScritta = 0;
     while (fgets(buff, BUFSIZ, fpData) != NULL) {
         //
         write(fdPipe, buff, strlen(buff));
         //
-        write(asfd, buff, strlen(buff));
+        write(sfd, buff, strlen(buff));
         //
         write(fpAppoggio, buff, strlen(buff));
         fsync(fpAppoggio);
