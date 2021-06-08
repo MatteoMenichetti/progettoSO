bf2ff6363c41d60fdfbd2621f639c523af9489d8
matteo <matteo.menichetti@stud.unifi.it>
2021-06-08 09:16:54 +0200

    completamento refactoring e aggiunta comportamento relativo a watchdog.c (scrittura dopo log del messaggio)

--- src/decision_function.c
+++ src/decision_function.c
@@ -1,22 +1,45 @@
 #include "../lib/ipc.h"
 
 #define LOGPOS "../logs/system_log"
-#define VOTEDO "../logs/voted_output"
-#define VOTEDP 4
+#define VOTEDPOS "../logs/voted_output"
+#define WATCHdPOS "../tmp/pipeWATCDOG"
 #define LOGVALUE 0
+#define VOTEDVALUE 4
+#define WATCHVALUE 5
 #define FALLIMENTO "FALLIMENTO \n"
 #define SUCCESSO "SUCCESSO \n"
+#define IMALIVE "IMALIVE"
 
-void writeOnLog(int fd, char *buffer) {
-    write(fd, buffer, strlen(buffer));
+void writeOnLog(int *fd, char *buffer) {
+    if ((write(fd[VOTEDVALUE], buffer, strlen(buffer))) == -1) {
+        perror("DF: write on voted_output");
+        exit(EXIT_FAILURE);
+    }
+    if ((write(fd[WATCHVALUE], IMALIVE, strlen(IMALIVE))) == -1) {
+        perror("DF: write on voted_output");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void openPIPE(int *fd) {
-
+void openFILE(int *fd) {
     if (((fd[LOGVALUE] = open(LOGPOS, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1)) {
         perror("DF: open log");
         exit(EXIT_FAILURE);
     }
+
+    if (!(fd[VOTEDVALUE] = open(VOTEDPOS, O_WRONLY | O_CREAT | O_TRUNC, 0666))) {
+        perror("DF: open voted_output");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void openPIPE(int *fd) {
+
+    if (((fd[WATCHVALUE] = open(WATCHdPOS, O_WRONLY))) == -1) {
+        perror("DF: open WATCHDOG pipe");
+        exit(EXIT_FAILURE);
+    }
+
     if ((fd[P1] = open(PIPEDP1, O_RDONLY)) == -1) {
         perror("DF: open pipedp1");
         exit(EXIT_FAILURE);
@@ -40,14 +63,20 @@ int EqualCondition(int vp1, int vp2, int vp3) {
 }
 
 int main(void) {
-    int fd[5], pid, vp[3], i = 1;
+    int fd[6], pid, vp[3], i = 1;
+
     if (!(pid = fork()))
-        execl("./failure_manager", "failure_manager", NULL);
-    openPIPE(fd);
-    if(!(fd[VOTEDP] = open(VOTEDO, O_WRONLY | O_CREAT | O_TRUNC, 0666))){
-        perror("DF: open voted_output");
-        exit(EXIT_FAILURE);
+        execl("./failure_manager", "failure_manager", (char *) NULL);
+
+    if (!(fork())) {
+        char *cpid = (char *) calloc(1, sizeof(pid));
+        sprintf(cpid, "%d", pid);
+        execl("./watchdog", "watchdog", cpid, (char *) NULL);
     }
+
+    openPIPE(fd);
+    openFILE(fd);
+
     while (0 == 0) {
         if ((read(fd[P1], (vp + P1 - 1), sizeof(int))) == -1) perror("DF: read P1");
 
@@ -55,14 +84,14 @@ int main(void) {
 
         if ((read(fd[P3], (vp + P3 - 1), sizeof(int))) == -1) perror("DF: read P3");
 
-        dprintf(fd[VOTEDP], "READ %d : p1 = %d p2 = %d p3 = %d \n", i, vp + P1 - 1, vp + P2 - 1, vp + P3 - 1);
+        dprintf(fd[VOTEDVALUE], "READ %d : p1 = %d p2 = %d p3 = %d \n", i, vp + P1 - 1, vp + P2 - 1, vp + P3 - 1);
 
         if (!EqualCondition(vp[P1 - 1], vp[P2 - 1], vp[P3 - 1])) {
-            writeOnLog(fd[LOGVALUE], FALLIMENTO);
+            writeOnLog(fd, FALLIMENTO);
             kill(pid, SIGUSR1);
         } else {
             i++;
-            writeOnLog(fd[LOGVALUE], SUCCESSO);
+            writeOnLog(fd, SUCCESSO);
         }
 
     }
