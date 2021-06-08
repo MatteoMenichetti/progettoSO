2bc264fdeeedc537c2ea7a3abf32af983534cc94
matteo <matteo.menichetti@stud.unifi.it>
2021-06-08 11:15:10 +0200

    rinominazione variabili

--- src/decision_function.c
+++ src/decision_function.c
@@ -71,36 +71,36 @@ int EqualCondition(int vp1, int vp2, int vp3) {
 }
 
 int main(void) {
-    int fd[6], pid, vp[3], i = 1;
+    int fd[6], failure_pid, valueSplitSum[3], i = 1;
 
     initializationPIPE();
 
     printf("DF: avvio FM\n");
-    if (!(pid = fork()))
+    if (!(failure_pid = fork()))
         execl("./failure_manager", "failure_manager", (char *) NULL);
 
     printf("DF: avvio watchdog\n");
     if (!(fork())) {
-        char *cpid = (char *) calloc(1, sizeof(pid));
-        sprintf(cpid, "%d", pid);
-        execl("./watchdog", "watchdog", cpid, (char *) NULL);
+        char *cFP = (char *) calloc(1, sizeof(failure_pid));
+        sprintf(cFP, "%d", failure_pid);
+        execl("./watchdog", "watchdog", cFP, (char *) NULL);
     }
 
     openPIPE(fd);
     openFILE(fd);
 
     while (0 == 0) {
-        if ((read(fd[P1], (vp + P1 - 1), sizeof(int))) == -1) perror("DF: read P1");
+        if ((read(fd[P1], (valueSplitSum + P1 - 1), sizeof(int))) == -1) perror("DF: read P1");
 
-        if ((read(fd[P2], (vp + P2 - 1), sizeof(int))) == -1) perror("DF: read P2");
+        if ((read(fd[P2], (valueSplitSum + P2 - 1), sizeof(int))) == -1) perror("DF: read P2");
 
-        if ((read(fd[P3], (vp + P3 - 1), sizeof(int))) == -1) perror("DF: read P3");
+        if ((read(fd[P3], (valueSplitSum + P3 - 1), sizeof(int))) == -1) perror("DF: read P3");
 
-        dprintf(fd[VOTEDVALUE], "READ %d : p1 = %d p2 = %d p3 = %d \n", i, vp[P1 - 1], vp[P2 - 1], vp[P3 - 1]);
+        dprintf(fd[VOTEDVALUE], "READ %d : p1 = %d p2 = %d p3 = %d \n", i, valueSplitSum[P1 - 1], valueSplitSum[P2 - 1], valueSplitSum[P3 - 1]);
 
-        if (!EqualCondition(vp[P1 - 1], vp[P2 - 1], vp[P3 - 1])) {
+        if (!EqualCondition(valueSplitSum[P1 - 1], valueSplitSum[P2 - 1], valueSplitSum[P3 - 1])) {
             writeOnLog(fd, FALLIMENTO);
-            kill(pid, SIGUSR1);
+            kill(failure_pid, SIGUSR1);
         } else {
             i++;
             writeOnLog(fd, SUCCESSO);
