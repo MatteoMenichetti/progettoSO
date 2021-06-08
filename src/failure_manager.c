ec78527d99ec0aa52a6d2c5191f49c77787bdec5
matteo <matteo.menichetti@stud.unifi.it>
2021-06-08 10:11:47 +0200

    modificato contenuto printf

--- src/failure_manager.c
+++ src/failure_manager.c
@@ -9,7 +9,7 @@ void funzione(int sig) {
 
 
 int main(int argc, char *argv[]) {
-    printf("%d\n", getpid());
+    printf("FM: pid = %d\n", getpid());
     signal(SIGUSR1, funzione);
     int status;
     do { status = pause(); } while (status != -1);
