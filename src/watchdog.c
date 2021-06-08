8705ce1ab7fb1996ca8eecaeb8eeaa70a6e0abd3
matteo <matteo.menichetti@stud.unifi.it>
2021-06-08 09:52:54 +0200

    pirma commit

---
+++ src/watchdog.c
@@ -0,0 +1,49 @@
+#include "../lib/ipc.h"
+
+int pid = 0;
+
+void handler() {
+    printf("watchdog: inivio SIGUSR1 a FM (%d)", pid);
+    kill(pid, SIGUSR1);
+}
+
+void initializationPIPE() {
+    if ((mknod(WATCHPPOS, S_IFIFO, DEFAULT)) == -1) {
+        perror("watchdog: mknod");
+        exit(EXIT_FAILURE);
+    }
+    chmod(WATCHPPOS, 0777);
+}
+
+void openPIPE(int *fd) {
+    while ((*fd = open(WATCHPPOS, O_RDONLY)) == -1) {
+        perror("watchdog: open WATCHDOGPIPE");
+        sleep(1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("watchdog: numero argomenti insuff.");
+        exit(EXIT_FAILURE);
+    }
+    pid = atoi(argv[1]);
+    int fd = -1;
+    char *buff = (char *) calloc(1, strlen(IMALIVE));
+
+    initializationPIPE();
+    openPIPE(&fd);
+
+    signal(SIGALRM, handler);
+
+    while (0 == 0) {
+        alarm(3);
+        if ((read(fd, buff, sizeof(buff))) == -1) {
+            perror("watchdog: lettura");
+            exit(EXIT_FAILURE);
+        }
+        if ((strcmp(buff, IMALIVE)) == 0) {
+            alarm(0);
+        }
+    }
+}
