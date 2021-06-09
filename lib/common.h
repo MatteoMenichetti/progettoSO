5ba5b4a3279be3f9a6a191ac04a63ecf79446cfa
matteo <matteo.menichetti@stud.unifi.it>
2021-06-09 21:33:15 +0200

    first commit

---
+++ lib/common.h
@@ -0,0 +1,7 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+
+#define PERMISIONFILE 0666
---
+++ src/ipc.c
@@ -0,0 +1,24 @@
+#include "../lib/ipc.h"
+
+void createPIPE(char *path[], int nrelements) {
+    for (int i = 0; i < nrelements; ++i) {
+        unlink(path[i]);
+        if ((mknod(path[i], S_IFIFO, DEFAULT)) == -1) {
+            perror("watchdog: mknod");
+            exit(EXIT_FAILURE);
+        }
+        chmod(path[i], PERMISSIONPIPE);
+    }
+
+}
+
+int *openPIPE(char *path[], int nrelements, int *flag) {
+    int *fd = (int *) calloc(nrelements, sizeof(int));
+    for (int i = 0; i < nrelements; i++)
+        if ((fd[i] = open(path[i], flag[i])) == -1) {
+            char message[100];
+            sprintf(message, "open %s", path[i]);
+            perror(message);
+        }
+    return fd;
+}
