b94a435121975275270dc52b2625faad6ed12be8
matteo <matteo.menichetti@stud.unifi.it>
2021-06-13 15:31:57 +0200

    aggiunte printf di segnalazione

--- src/main.c
+++ src/main.c
@@ -27,8 +27,10 @@ int main(int argc, char *argv[]) {
 
     int pid_decision = fork();
     if (pid_decision == 0) {
+        printf("main: avvio DF\n");
         execl("./decision_function", "decision_function", NULL);
     } else {
+        printf("main: avvio IM\n");
         execl("./input_manager", "input_manger", argv[2], NULL);
     }
 }
