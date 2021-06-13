0e5d60f434bd17c10c73ffe67b31dcffe1df1aa7
matteo <matteo.menichetti@stud.unifi.it>
2021-06-13 15:31:47 +0200

    aggiunte printf di segnalazione

--- src/ipc.c
+++ src/ipc.c
@@ -3,10 +3,14 @@
 void createPIPE(char *path[], int nrelements) {
     for (int i = 0; i < nrelements; i++) {
         unlink(path[i]);
-        if ((mknod(path[i], S_IFIFO, DEFAULT)) == -1) {
-            perror("watchdog: mknod");
+        int mkresult;
+        if ((mkresult = mknod(path[i], S_IFIFO, DEFAULT)) == -1) {
+            char message[100];
+            sprintf(message, "open %s", path[i]);
+            perror(message);
             exit(EXIT_FAILURE);
         }
+        printf("IPC: mkresult = %d su %s", mkresult, path[i]);
         chmod(path[i], PERMISSIONPIPE);
     }
 
@@ -14,12 +18,16 @@ void createPIPE(char *path[], int nrelements) {
 
 int *openPIPE(char *path[], int nrelements, int *flag) {
     int *fd = (int *) calloc(nrelements, sizeof(int));
-    for (int i = 0; i < nrelements; i++)
+    printf("nrelements = %d\n", nrelements);
+    for (int i = 0; i < nrelements; i++) {
+        printf("IPC: open (%d) %s with flag %d", i, path[i], flag[i]);
         if ((fd[i] = open(path[i], flag[i])) == -1) {
             char message[100];
             sprintf(message, "open %s", path[i]);
             perror(message);
             exit(EXIT_FAILURE);
         }
+        printf(" with result %d\n", fd[i]);
+    }
     return fd;
 }
