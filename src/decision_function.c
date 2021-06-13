bf327f541cc7524137441c21fae864ffefbd3686
matteo <matteo.menichetti@stud.unifi.it>
2021-06-13 15:30:22 +0200

    diminuzione dei valori associati alle posizione dei comèonenti utilizzati per l'array fd e rimossa gestione delle pipe legate a watchdog per il passaggio ai segnali

--- src/decision_function.c
+++ src/decision_function.c
@@ -3,23 +3,19 @@
 #define LOGPATH "../logs/system_log"
 #define VOTEDPATH "../logs/voted_output"
 
-#define WATCHPOS 4
-#define VOTEDPOS 5
-#define LOGPOS 6
+#define VOTEDPOS 3
+#define LOGPOS 4
 
 #define FALLIMENTO "FALLIMENTO \n"
 #define SUCCESSO "SUCCESSO \n"
 
-void writeOnLog(int *fd, char *buffer) {
-    if ((write(fd[VOTEDPOS], buffer, strlen(buffer))) == -1) {
+void writeOnLog(int fd, int pid_failure_manager, char *buffer) {
+    if ((write(fd, buffer, strlen(buffer))) == -1) {
         perror("DF: write on voted_output");
         exit(EXIT_FAILURE);
     }
 
-    if ((write(fd[WATCHPOS], IMALIVE, strlen(IMALIVE))) == -1) {
-        perror("DF: write on voted_output");
-        exit(EXIT_FAILURE);
-    }
+    kill(pid_failure_manager, SIGUSR1);
 }
 
 void openFILE(int *fd) {
@@ -40,21 +36,16 @@ int EqualCondition(int vp1, int vp2, int vp3) {
     return 0;
 }
 
-void createPIPES() {
-    char *pipes[4] = {WATCHPATH, PIPEDP1PATH, PIPEDP2PATH, PIPEDP3PATH};
-    createPIPE(pipes, 4);
-}
-
 void openPIPES(int fd[]) {
-    char *paths[4] = {PIPEDP1PATH, PIPEDP2PATH, PIPEDP3PATH, WATCHPATH};
-    int flags[4] = {O_WRONLY, O_RDONLY, O_RDONLY, O_RDONLY};
-    fd = openPIPE(paths, 4, flags);
+    char *paths[3] = {PIPEDP1PATH, PIPEDP2PATH, PIPEDP3PATH};
+    printf("DF: *path[0] = %s , *path[1] = %s, *path[2] = %s\n", paths[0], paths[1], paths[2]);
+    int flags[3] = {O_RDONLY, O_RDONLY, O_RDONLY};
+    fd = openPIPE(paths, 3, flags);
+    printf("DF: apertura PIPES %d %d %d %d\n", fd[0], fd[1], fd[2], fd[3]);
 }
 
 int main(void) {
-    int fd[7], failure_pid, valueSplitSum[3], i = 1;
-
-    createPIPES();
+    int fd[5], failure_pid, watchdog_pid, valueSplitSum[3], i = 1;
 
     printf("DF: avvio FM\n");
     if (!(failure_pid = fork()))
@@ -62,32 +53,40 @@ int main(void) {
 
     printf("DF: avvio watchdog\n");
 
-    if (!(fork())) {
+    if (!(watchdog_pid = fork())) {
         char *cFP = (char *) calloc(1, sizeof(failure_pid));
         sprintf(cFP, "%d", failure_pid);
         execl("./watchdog", "watchdog", cFP, (char *) NULL);
     }
 
-    openPIPES(fd);
+    printf("DF: open FILE\n");
     openFILE(fd);
+    printf("DF: open PIPES\n");
+    openPIPES(fd);
+    printf("DF: fd[P1] = %d, fd[P2] = %d, fd[P3] = %d, fd[LOGPOS] = %d, fd[VOTEDPOS] = %d\n", fd[P1], fd[P2], fd[P3],
+           fd[LOGPOS], fd[VOTEDPOS]);
 
     while (0 == 0) {
+        printf("DF: read");
         if ((read(fd[P1 - 1], (valueSplitSum + P1 - 1), sizeof(int))) == -1) perror("DF: read P1");
 
         if ((read(fd[P2 - 1], (valueSplitSum + P2 - 1), sizeof(int))) == -1) perror("DF: read P2");
 
         if ((read(fd[P3 - 1], (valueSplitSum + P3 - 1), sizeof(int))) == -1) perror("DF: read P3");
-
+        printf("DF:  read fd[P1] = %s, fd[P2] = %s, fd[P3] = %s\n", valueSplitSum + P1 - 1, valueSplitSum + P2 - 1,
+               valueSplitSum + P3 - 1);
         dprintf(fd[LOGPOS], "READ %d : p1 = %d p2 = %d p3 = %d \n", i, valueSplitSum[P1 - 1],
                 valueSplitSum[P2 - 1],
                 valueSplitSum[P3 - 1]);
 
         if (!EqualCondition(valueSplitSum[P1 - 1], valueSplitSum[P2 - 1], valueSplitSum[P3 - 1])) {
-            writeOnLog(fd, FALLIMENTO);
+            writeOnLog(fd[VOTEDPOS], watchdog_pid, FALLIMENTO);
+            printf("DF: FALLIMENTO\n");
             kill(failure_pid, SIGUSR1);
         } else {
             i++;
-            writeOnLog(fd, SUCCESSO);
+            writeOnLog(fd[VOTEDPOS], watchdog_pid, SUCCESSO);
+            printf("DF: SUCCESSO\n");
         }
 
     }
