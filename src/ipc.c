c3524b88e1773f97c1b5b02125cc8eb72f8e7aee
matteo <matteo.menichetti@stud.unifi.it>
2021-06-15 21:27:21 +0200

    aggiornata apertura e creazione pipe una alla volta

--- src/ipc.c
+++ src/ipc.c
@@ -1,32 +1,24 @@
 #include "../lib/ipc.h"
 
-void createPIPE(char *path[], int nrelements) {
-    for (int i = 0; i < nrelements; i++) {
-        unlink(path[i]);
-        int mkresult;
-        if ((mkresult = mknod(path[i], S_IFIFO, DEFAULT)) == -1) {
-            char message[100];
-            sprintf(message, "open %s", path[i]);
-            perror(message);
-            exit(EXIT_FAILURE);
-        }
-        printf("IPC: mkresult = %d su %s", mkresult, path[i]);
-        chmod(path[i], PERMISSIONPIPE);
+void createPIPE(char *pipe) {
+    unlink(pipe);
+    if (mknod(pipe, S_IFIFO, DEFAULT) == -1) {
+        perror("IPC: mknod");
+        exit(EXIT_FAILURE);
     }
+    chmod(pipe, PERMISSIONPIPE);
 }
 
-int *openPIPE(char *path[], int nrelements, int *flag) {
-    int *fd = (int *) calloc(nrelements, sizeof(int));
-    printf("nrelements = %d\n", nrelements);
-    for (int i = 0; i < nrelements; i++) {
-        printf("IPC: open (%d) %s with flag %d", i, path[i], flag[i]);
-        if ((fd[i] = open(path[i], flag[i])) == -1) {
-            char message[100];
-            sprintf(message, "open %s", path[i]);
-            perror(message);
-            exit(EXIT_FAILURE);
-        }
-        printf(" with result %d\n", fd[i]);
+int openPIPE(char path[], int flag) {
+    int fd;
+    printf("IPC: open %s with flag %d", path, flag);
+    if ((fd = open(path, flag)) == -1) {
+        char message[100];
+        sprintf(message, "open %s", path);
+        perror(message);
+        exit(EXIT_FAILURE);
     }
+    printf(" with result %d\n", fd);
+
     return fd;
 }
