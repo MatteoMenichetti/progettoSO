57b8cf08e3ae7104510e1ee97a7fb6b9e16d24c2
matteo <matteo.menichetti@stud.unifi.it>
2021-06-16 11:59:38 +0200

    rimozione vecchia implementazione p1

--- src/p1.c
+++ src/p1.c
@@ -1,41 +1,5 @@
 #include "../lib/p.h"
 
-/*void p1(int modality, int pid) {
-    int flag[1] = {O_WRONLY};
-    char *path[1] = {PIPEDP1PATH};
-
-    printf("P1: creazione PIPEDP1\n");
-    createPIPE(path, 1);
-
-    int kills = kill(pid, SIGCONT);
-
-    printf("P1: open PIPEDP1\n");
-    int psfd = *openPIPE(path, 1, flag);
-
-    int pfd = open(PIPEPATH, O_RDONLY);
-
-    char buff[BUFSIZ];
-    int s = 0, r = 0;
-    while (0 == 0) {
-        if((r = read(pfd, buff, sizeof(buff)))==-1) {
-            perror("P1: read su pipe");
-            exit(EXIT_FAILURE);
-        }
-        printf("P1: splitP1\n");
-        splitP1(buff, &s);
-
-        if (modality == ACTIVE_FAILURE)errsum(&s, 10);
-
-        printf("P1: invio a DF %d\n", s);
-        if (write(psfd, &s, sizeof(s)) == -1) {
-            perror("P1: write");
-            exit(EXIT_FAILURE);
-        }
-        strncpy(buff, "\0", r);
-
-        s = 0;
-    }
-}*/
 void p1(int modality, int pid) {
     createPIPE(PIPEDP1PATH);
 
