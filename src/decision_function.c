163aa3a283383fa6256c4c181328a28c42e75078
matteo <matteo.menichetti@stud.unifi.it>
2021-06-16 10:31:31 +0200

    effetuato refactoring sul metodo writeOnLog. creato controllValueRecive per decidere quale valore scrivere sui log file e quali segnali inviare. i controlli sui valori ricevuti passano dal main a controllValueRecicede write on log mantiene solito comportamento. definizione di un vettore per i pid (con macro conseguenti per i processi watchdog e failure_manager). ridenominazione pid inviato a watchdog

--- src/decision_function.c
+++ src/decision_function.c
@@ -3,21 +3,50 @@
 #define LOGPATH "../logs/system_log"
 #define VOTEDPATH "../logs/voted_output"
 
+#define WATCHDOG 0
+#define FAILURE_MANAGER 1
 #define VOTEDPOS 3
 #define LOGPOS 4
 
-#define FALLIMENTO "\nFALLIMENTO"
-#define SUCCESSO "\nSUCCESSO"
+#define FALLIMENTO "FALLIMENTO\n"
+#define SUCCESSO "SUCCESSO\n"
 
-void writeOnLog(int fd, int pid_failure_manager, char *buffer) {
-    if ((write(fd, buffer, strlen(buffer))) == -1) {
+void writeOnLog(int fd[], int valueSplitSum[], char buffer[]){
+    dprintf(fd[LOGPOS], "p1 = %d p2 = %d p3 = %d \n", valueSplitSum[P1 - 1],
+            valueSplitSum[P2 - 1],
+            valueSplitSum[P3 - 1]);
+
+    if ((write(fd[VOTEDPOS], buffer, strlen(buffer))) == -1) {
         perror("DF: write on voted_output");
         exit(EXIT_FAILURE);
     }
-    if ((kill(pid_failure_manager, SIGUSR1)) == -1) {
-        perror("decision_function: kill on failure_manager");
+}
+
+int EqualityCondition(int vp1, int vp2, int vp3) {
+    if (vp1 != vp2 && vp1 != vp3 && vp2 != vp3)return 0;
+    return 1;
+}
+
+void controllValueRecived(int *fd, int *pid, int *valueSplitSum) {
+    char *buffer;
+    printf("decision_function: valueSplitSum[P1 - 1] = %d, valueSplitSum[P2 - 1] = %d, valueSplitSum[P3 - 1] = %d\n",
+           valueSplitSum[P1 - 1], valueSplitSum[P2 - 1], valueSplitSum[P3 - 1]);//eliminare
+    if (!EqualityCondition(valueSplitSum[P1 - 1], valueSplitSum[P2 - 1], valueSplitSum[P3 - 1]))
+        buffer = FALLIMENTO;
+    else buffer = SUCCESSO;
+
+    writeOnLog(fd, valueSplitSum, buffer);
+
+    if ((strcmp(buffer, FALLIMENTO)) == 0) {
+        printf("decision_function: send SIGUSR1 to failure_manager (FALLIMENTO)\n");
+        kill(pid[FAILURE_MANAGER], SIGUSR1);
+    }
+    printf("decision_function: send signal (I_AM_ALIVE) to watchdog\n");
+    if ((kill(pid[WATCHDOG], SIGUSR1)) == -1) {
+        perror("decision_function: kill on watchdog");
         exit(EXIT_FAILURE);
     }
+
 }
 
 void openFILE(int *fd) {
@@ -33,11 +62,6 @@ void openFILE(int *fd) {
     printf("decision_function: fd[LOGPOS] = %d, fd[VOTEDPOS] = %d\n", fd[LOGPOS], fd[VOTEDPOS]);
 }
 
-int EqualityCondition(int vp1, int vp2, int vp3) {
-    if (vp1 != vp2 && vp1 != vp3 && vp2 != vp3)return 0;
-    return 1;
-}
-
 void openPIPES(int fd[]) {
     char *paths[3] = {PIPEDP1PATH, PIPEDP2PATH, PIPEDP3PATH};
     printf("DF: *path[0] = %s , *path[1] = %s, *path[2] = %s\n", paths[0], paths[1], paths[2]);
@@ -49,18 +73,19 @@ void openPIPES(int fd[]) {
 
 int main(void) {
     signal(SIGUSR1, SIG_IGN);
-    int fd[5], failure_pid, watchdog_pid, valueSplitSum[3], i = 1;
+
+    int fd[5], pid[2], valueSplitSum[3];
 
     printf("DF: avvio FM\n");
-    if (!(failure_pid = fork()))
-        execl("./failure_manager", "failure_manager", (char *) NULL);
+    if (!(pid[FAILURE_MANAGER] = fork()))
+        execl("./failure_manager", (char *) NULL);
 
     printf("DF: avvio watchdog\n");
 
-    if (!(watchdog_pid = fork())) {
-        char *cFP = (char *) calloc(1, sizeof(failure_pid));
-        sprintf(cFP, "%d", failure_pid);
-        execl("./watchdog", "watchdog", cFP, (char *) NULL);
+    if (!(pid[WATCHDOG] = fork())) {
+        char *failure_manager_pid = (char *) calloc(1, sizeof(pid[FAILURE_MANAGER]));
+        sprintf(failure_manager_pid, "%d", pid[FAILURE_MANAGER]);
+        execl("./watchdog", "watchdog", failure_manager_pid, (char *) NULL);
     }
 
     printf("DF: open FILE\n");
@@ -68,30 +93,19 @@ int main(void) {
     printf("DF: open PIPES\n");
     openPIPES(fd);
     printf("DF: fd[P1] = %d, fd[P2] = %d, fd[P3] = %d, fd[LOGPOS] = %d, fd[VOTEDPOS] = %d\n", fd[P1], fd[P2], fd[P3],
-           fd[LOGPOS], fd[VOTEDPOS]);
+           fd[LOGPOS], fd[VOTEDPOS]);//eliminare
 
     while (0 == 0) {
-        printf("DF: read\n");
+        printf("decision_function: read from p pipe\n");
         if ((read(fd[P1 - 1], &valueSplitSum[P1 - 1], sizeof(int))) == -1) perror("DF: read P1");
 
         if ((read(fd[P2 - 1], &valueSplitSum[P2 - 1], sizeof(int))) == -1) perror("DF: read P2");
 
         if ((read(fd[P3 - 1], &valueSplitSum[P3 - 1], sizeof(int))) == -1) perror("DF: read P3");
         printf("DF:  read fd[P1] = %d, fd[P2] = %d, fd[P3] = %d\n", valueSplitSum[P1 - 1], valueSplitSum[P2 - 1],
-               valueSplitSum[P3 - 1]);
-        dprintf(fd[LOGPOS], "READ %d : p1 = %d p2 = %d p3 = %d \n", i, valueSplitSum[P1 - 1],
-                valueSplitSum[P2 - 1],
-                valueSplitSum[P3 - 1]);
+               valueSplitSum[P3 - 1]);//eliminare
 
-        if (!EqualityCondition(valueSplitSum[P1 - 1], valueSplitSum[P2 - 1], valueSplitSum[P3 - 1])) {
-            writeOnLog(fd[VOTEDPOS], watchdog_pid, FALLIMENTO);
-            printf("DF: FALLIMENTO\n");
-            kill(failure_pid, SIGUSR1);
-        } else {
-            i++;
-            writeOnLog(fd[VOTEDPOS], watchdog_pid, SUCCESSO);
-            printf("DF: SUCCESSO\n");
-        }
+        controllValueRecived(fd, pid, valueSplitSum);
 
     }
 }
