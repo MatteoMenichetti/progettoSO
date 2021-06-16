9a542d8fd5d58f403edb1f78af089b527cf501ba
matteo <matteo.menichetti@stud.unifi.it>
2021-06-16 12:07:46 +0200

    dichiarazione variabili in un unico punto (cambiato tipo di res per essere consorde con almeno una funzione (strlen o fseek). formattazione codice e modifica messaggi.

--- src/p3.c
+++ src/p3.c
@@ -2,12 +2,16 @@
 
 void p3(int modality, int pid) {
     createPIPE(PIPEDP3PATH);
+
     kill(pid, SIGCONT);
-    int psfd, flag = O_WRONLY;
-    char *path = {PIPEDP3PATH};
-    //psfd=*openPIPE(&path, 1, &flag );
-    psfd = openPIPE(path, flag);
+
+    int psfd, flag = O_WRONLY, s = 0;
+    unsigned long res = 0;
+
+    char *path = {PIPEDP3PATH}, buff[BUFSIZ], *token = (char *) malloc(sizeof(char));
+
     printf("P3: open PIPEDP3\n");
+    psfd = openPIPE(path, flag);
 
     FILE *fd = fopen(FILEPATH, "r+");
 
@@ -16,22 +20,23 @@ void p3(int modality, int pid) {
         exit(EXIT_FAILURE);
     }
 
-    char buff[BUFSIZ], *token = (char *) malloc(sizeof(char));
-    int s = 0, res = 0;
-
     while (0 == 0) {
         if (fgets(buff, sizeof(buff), fd)) {
             res += strlen(buff);
+
             printf("P3: splitP2\n");
             token = splitP2(buff);
+
             s = sum(token, 0);
             if (modality == ACTIVE_FAILURE)errsum(&s, 30);
-            printf("P3: invio a DF %d\n", s);
+
+            printf("P3: send to DF %d\n", s);
             if (write(psfd, &s, sizeof(s)) == -1) {
                 perror("P3: write");
                 exit(EXIT_FAILURE);
             }
-            printf("P3: %d \n", s);
+
+            strncpy(buff, "\0", strlen(buff));
         }
         fseek(fd, res, SEEK_SET);
     }
