e42338b01e23bc9859cb4cec9515fa27bd2738fe
matteo <matteo.menichetti@stud.unifi.it>
2021-06-16 11:11:18 +0200

    rinominazione significativa varibili, modifica printf e scambio apertura pipe e socket per evitare connection refused

--- src/input_manager.c
+++ src/input_manager.c
@@ -1,70 +1,72 @@
 #include "../lib/ipc.h"
 
 int initializationSOCKET(struct sockaddr_un *sockServer) {
-    int fd;
+    int sfd;
 
     strcpy(sockServer->sun_path, SOCKPATH);
     sockServer->sun_family = AF_UNIX;
 
     unlink(SOCKPATH);
 
-    if ((fd = socket(AF_UNIX, SOCK_STREAM, DEFAULT)) == -1) {
-        perror("IM: socket");
+    if ((sfd = socket(AF_UNIX, SOCK_STREAM, DEFAULT)) == -1) {
+        perror("input_manager: socket");
         exit(EXIT_FAILURE);
     }
 
-    if (bind(fd, (struct sockaddr *) sockServer, sizeof(*sockServer)) == -1) {
-        perror("IM: bind");
+    if (bind(sfd, (struct sockaddr *) sockServer, sizeof(*sockServer)) == -1) {
+        perror("input_manager: bind");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(fd, 1) == -1) {
-        perror("IM: listen");
+    if (listen(sfd, 1) == -1) {
+        perror("input_manager: listen");
         exit(EXIT_FAILURE);
     }
 
-    return fd;
+    return sfd;
 }
 
-void acceptSOCKET(int *sfd) {
+//asfd = anonymous socket fd
+void acceptSOCKET(int *asfd) {
     struct sockaddr_un sockServer, socketClient;
     int ssfd = initializationSOCKET(&sockServer);
     unsigned int len = sizeof(socketClient);
 
-    if ((*sfd = accept(ssfd, (struct sockaddr *) &socketClient, &len)) == -1) {
-        perror("server: accept");
+    if ((*asfd = accept(ssfd, (struct sockaddr *) &socketClient, &len)) == -1) {
+        perror("input_manager: accept");
         exit(EXIT_FAILURE);
     }
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("input_manager: numero argomenti insuff.\n");
+        printf("input_manager: less than 2 arguments\n");
         exit(EXIT_FAILURE);
     }
-    char buff[BUFSIZ], *path= {PIPEPATH};
-    int flag =O_WRONLY, fdPipe, fpAppoggio = open(FILEPATH,
-                                                        O_CREAT | O_WRONLY |
-                                                        O_TRUNC,
-                                                        PERMISSIONFILE), sfd;
+    char buff[BUFSIZ], *path = {PIPEPATH};
+    int flag = O_WRONLY, fdPipe, fpAppoggio = open(FILEPATH,
+                                                   O_CREAT | O_WRONLY |
+                                                   O_TRUNC,
+                                                   PERMISSIONFILE), sfd;
+
     FILE *fpData = fopen(argv[1], "r");
 
-    printf("IM: create PIPE\n");
+    printf("input_manager: create PIPE\n");
 
     createPIPE(PIPEPATH);
 
-    printf("IM: open PIPE\n");
+    printf("input_manager: accept\n");
+    acceptSOCKET(&sfd);
+
+    printf("input_manager: open PIPE\n");
 
     fdPipe = openPIPE(path, flag);
-
-    printf("IM: accept\n");
-    acceptSOCKET(&sfd);
 
     fgets(buff, BUFSIZ, fpData);
     strncpy(buff, "\0", strlen(buff));
 
     while (fgets(buff, BUFSIZ, fpData) != NULL) {
-        printf("IM: write for P\n");
+        printf("input_manager: write for P\n");
         //
         write(fdPipe, buff, strlen(buff));
         //
